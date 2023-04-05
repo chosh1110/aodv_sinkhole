cb0b09ff785c14a4c149ac97a5ace948bcf2f83b
chosh1110 <chosh1110@naver.com>
2023-04-05 17:22:56 +0900

    move, hello_packet 업데이트

--- aodv/aodv.cpp
+++ aodv/aodv.cpp
@@ -11,15 +11,17 @@
 #include <random>
 #include <stdlib.h>
 #include <queue>
+#include <mutex>
 using namespace std;
 const int NETWORK_SIZE = 50;
 const int NODE_COUNT = 10;
-const int TRANSMISSION_RANGE = 15;
+const double TRANSMISSION_RANGE = 15.0;
+mutex mtx;
 struct RREQ {
-    int hop;
-    int des_ip;
+    int hop = 0;
+    int des_ip; //목적지 노드의 id 대체
     int des_seq;
-    int org_ip;
+    int org_ip; //출발지 노드의 id 대체
     int org_seq;
 };
 struct RREP {
@@ -33,11 +35,15 @@ struct RERR {
     int un_dest_ip;
     int un_dest_seq;
 };
+
 class Node {
     int node_x;
     int node_y;
     int id;
     vector <int> neighbor;
+    queue <RREQ> buffer;
+    condition_variable cv;
+    mutex cv_m;
 public:
     Node(int node_id) {
         node_x = rand() % NETWORK_SIZE;
@@ -61,7 +67,7 @@ public:
     }
     void move(const vector<Node*>& nodes) {
         static default_random_engine generator;
-        static uniform_int_distribution<int> distribution(-3, 3);
+        static uniform_int_distribution<int> distribution(-1,1);
 
         // Calculate new position
         int new_x = node_x + distribution(generator);
@@ -94,11 +100,53 @@ public:
         this->neighbor.clear();
         for (auto node : nodes) {
             if (node != this) {
-                int distance = sqrt(pow(node->node_x - this->node_x, 2) + pow(node->node_y - this->node_y, 2));
+                double distance = sqrt(pow(node->node_x - this->node_x, 2) + pow(node->node_y - this->node_y, 2));
+                printf("S: %2d D: %2d Distance:%f\n", this->getId(), node->getId(),distance);
                 if (distance <= TRANSMISSION_RANGE) {
-                    this->neighbor.push_back(node->id);
+                    this->neighbor.push_back(node->id);                    
                 }
             }
+        }
+    }
+    void send_rreq(const vector<Node*>& nodes) {
+        RREQ rreq;
+        static default_random_engine generator;
+        static uniform_int_distribution<int> distribution(0, 100);
+        int num = distribution(generator);
+        if (num > 80) {
+            static uniform_int_distribution<int> distribution(0, NODE_COUNT-1);
+            rreq.hop += 1;
+            for (int i = 0; i < NODE_COUNT; i++)
+            {
+                int dest_ip = distribution(generator);
+                if (dest_ip == this->getId()) {
+                    continue;
+                }
+                else {
+                    rreq.des_ip = dest_ip;
+                    break;
+                }
+            }
+            rreq.des_seq = 15;
+            rreq.org_ip = this->getId();
+            rreq.org_seq = 23;
+
+            for (auto neighbor : this->getNeighbor()) {
+                for (auto node : nodes) {
+                    if (neighbor == node->getId()) {
+                        node->receive_rreq(rreq);
+                        printf("Source :%2d | Destination:%2d | RREQ_hop:%2d | RREQ_des_ip:%2d | RREQ_des_seq:%2d | RREQ_org_ip:%2d | RREQ_org_seq:%2d\n", this->getId(), node->getId(), rreq.hop, rreq.des_ip, rreq.des_seq, rreq.org_ip, rreq.org_seq);
+                    }
+                }
+            }
+        }
+    }
+    void receive_rreq(RREQ rreq) {
+        if (rreq.des_ip == this->getId()) {
+            //목적지 도착
+        }
+        else {
+            this->buffer.push(rreq);
         }
     }
 };
@@ -134,12 +182,22 @@ int main() {
     for (int i = 0; i < NODE_COUNT; i++) {
         nodes.push_back(new Node(i));
     }
-
     while (1) {
         vector<thread> threads;
+        
+        for (auto node : nodes) {
+            threads.push_back(thread(&Node::move, node, ref(nodes)));
+            //threads.push_back(thread(&Node::send_rreq, node, ref(nodes)));
+        }
         for (auto node : nodes) {
             threads.push_back(thread(&Node::hello_packet, node, ref(nodes)));
+            //threads.push_back(thread(&Node::send_rreq, node, ref(nodes)));
         }
+
+        for (auto& thread : threads) {
+            thread.join();
+        }
+
         for (auto node : nodes) {
             printf("Node Id:%2d | X:%3d | Y:%3d | Neighbor:", node->getId(), node->getCurrent_x(), node->getCurrent_y());
             for (int i = 0; i < node->getNeighbor().size(); i++)
@@ -149,14 +207,8 @@ int main() {
             printf("\n");
         }
         printNetwork(nodes);
-        for (auto node : nodes) {
-            threads.push_back(thread(&Node::move, node, ref(nodes)));
-        }
-        for (auto& thread : threads) {
-            thread.join();
-        }
         cout << endl;
-        system("cls");
+        Sleep(500);
     }
 
     return 0;
